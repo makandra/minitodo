d559372d01907808c7a078f77acf2160b0a35bf7
Henning Koch <henning.koch@makandra.de>
2018-10-22 12:13:49 +0200

    Use parens in app_spec

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -4,31 +4,30 @@ Capybara.app = Sinatra::Application
 
 feature 'Task management' do
   scenario 'User adds tasks, marks them as completed and clears completed tasks' do
-    visit '/'
+    visit('/')
 
-    fill_in 'task', with: 'Milk'
-    click_button 'Add'
-    fill_in 'task', with: 'Toast'
-    click_button 'Add'
-    fill_in 'task', with: 'Honey'
-    click_button 'Add'
+    fill_in('task', with: 'Milk')
+    click_button('Add')
+    fill_in('task', with: 'Toast')
+    click_button('Add')
+    fill_in('task', with: 'Honey')
+    click_button('Add')
 
     expect(page).to have_content('Milk')
     expect(page).to have_content('Toast')
     expect(page).to have_content('Honey')
 
-    click_link 'Toast'
-    click_link 'Honey'
+    click_link('Toast')
+    click_link('Honey')
 
     expect(page).to have_css('.task.open', text: 'Milk')
     expect(page).to have_css('.task.completed', text: 'Honey')
     expect(page).to have_css('.task.completed', text: 'Toast')
 
-    click_link 'Clear completed'
+    click_link('Clear completed')
 
     expect(page).to have_content('Milk')
     expect(page).to_not have_content('Honey')
     expect(page).to_not have_content('Toast')
-
   end
 end
