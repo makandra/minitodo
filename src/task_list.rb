ad434462ddd30762a36dd6f89f6ae3149de6f303
Henning Koch <henning.koch@makandra.de>
2018-10-24 15:17:22 +0200

    Remove implementation and tests for TaskList so we can demo it to the students

--- src/task_list.rb
+++ src/task_list.rb
@@ -1,30 +1,3 @@
 class TaskList
 
-  def initialize
-    @open_tasks = []
-    @completed_tasks = []
-  end
-  
-  def open_tasks
-    @open_tasks
-  end
-
-  def completed_tasks
-    @completed_tasks
-  end
-  
-  def add(task)
-    @open_tasks.push(task)
-  end
-  
-  def complete(task)
-    @open_tasks.include?(task) or raise 'No such task'
-    @open_tasks.delete(task)
-    @completed_tasks.unshift(task)
-  end
-  
-  def clear_completed
-    @completed_tasks = []
-  end
-
 end
